e4235810a16858f68716bc4db7532a0679a00767
MolchanovAlexander <rybkolhoz@gmail.com>
2025-05-21 12:12:43 +0300

    1st video

--- main.py
+++ main.py
@@ -1,4 +1,24 @@
+import cv2 as cv
 
 
-a = 1
-print(a)
+print(cv.__version__)
+#video = cv.VideoCapture(0) # first camera video input enabled
+video = cv.VideoCapture("ich.mp4")
+subtractor = cv.createBackgroundSubtractorMOG2(100, 50)
+
+while True:
+
+    ret, frame = video.read()
+
+    if ret:
+        mask = subtractor.apply(frame)
+        cv.imshow('Mask', mask)
+
+        if cv.waitKey(5) == ord("x"):
+            break
+
+    else:
+        video = cv.VideoCapture("ich.mp4")
+
+cv.destroyAllWindows()
+video.release()
